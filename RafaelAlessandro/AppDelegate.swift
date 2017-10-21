5bac748f6d9836bf2f74020ee3cb48ca2b21d7bc
Usuário Convidado <Guest@pa06micro39.local>
2017-10-21 12:30:32 -0200

    Correção do AppDelegate

--- RafaelAlessandro/AppDelegate.swift
+++ RafaelAlessandro/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     lazy var persistentContainer: NSPersistentContainer = {
         
-        let container = NSPersistentContainer(name: "RafaelAlessandro")
+        let container = NSPersistentContainer(name: "Model")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             //            if let error = error as NSError? {
             //                fatalError("Unresolved error \(error), \(error.userInfo)")
