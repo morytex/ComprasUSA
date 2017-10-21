48162e5abbb9fdf357f7c5bf9818526a06c28151
Usuário Convidado <Guest@pa06micro35.local>
2017-10-21 14:35:52 -0200

    Incluindo os campos na tela de registro

--- RafaelAlessandro/Product/ProductRegisterViewController.swift
+++ RafaelAlessandro/Product/ProductRegisterViewController.swift
@@ -12,13 +12,48 @@ import CoreData
 class ProductRegisterViewController: UIViewController {
     
     var product: Product!
+    var smallImage: UIImage!
+    var pickerView: UIPickerView!
     
-    @IBOutlet weak var lbName: UITextField!
+    var dataSource: [State] = []
+    
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var ivPicture: UIImageView!
+    @IBOutlet weak var tfStateName: UITextField!
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var swIsCreditCardPayment: UISwitch!
+    @IBOutlet weak var btnAddUpdate: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        tfName.placeholder = "Nome do produto"
+        tfStateName.placeholder = "Estado da compra"
+        tfValue.placeholder = "Valor (U$)"
+        btnAddUpdate.titleLabel?.text = "CADASTRAR"
+        
+        if product != nil{
+            btnAddUpdate.titleLabel?.text = "ATUALIZAR"
+
+            tfName.text = product.name
+            //            if let states = produto.states {
+            //                tfEstadoProduto.text = states.map({($0 as! State).nome!})
+            //            } else {
+            //                print("o estado está vazio")
+            //            }
+            
+            // tfValor.text = String( produto.valor )
+            // swCartao.setOn(produto.cartao, animated: false)
+            // btCadastrar.setTitle("Atualizar", for: .normal)
+            if let picture = product.picture as? UIImage {
+                ivPicture.image = picture
+            }
+        }
+        
+        setupPickerView()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ProductRegisterViewController.handleTap))
+        ivPicture.addGestureRecognizer(tapGesture)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,15 +61,122 @@ class ProductRegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupPickerView() {
+        pickerView = UIPickerView() //Instanciando o UIPickerView
+        pickerView.backgroundColor = .white
+        pickerView.delegate = self  //Definindo seu delegate
+        pickerView.dataSource = self  //Definindo seu dataSource
+        
+        //Criando uma toobar que servirá de apoio ao pickerView. Através dela, o usuário poderá
+        //confirmar sua seleção ou cancelar
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 44))
+        
+        //O botão abaixo servirá para o usuário cancelar a escolha de gênero, chamando o método cancel
+        let btCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let btSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        //O botão done confirmará a escolha do usuário, chamando o método done.
+        let btDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.items = [btCancel, btSpace, btDone]
+        
+        //Aqui definimos que o pickerView será usado como entrada do extField
+        tfStateName.inputView = pickerView
+        
+        //Definindo a toolbar como view de apoio do textField (view que fica acima do teclado)
+        tfStateName.inputAccessoryView = toolbar
     }
-    */
+    
+    func cancel() {
+        tfStateName.resignFirstResponder()
+    }
+    
+    func done() {
+        tfStateName.text = dataSource[pickerView.selectedRow(inComponent: 0)].name
+        
+        cancel()
+    }
 
+    func handleTap() {
+        //Criando o alerta que será apresentado ao usuário
+        let alert = UIAlertController(title: "Selecionar imagem", message: "De onde você quer escolher o imagem?", preferredStyle: .actionSheet)
+        
+        //Verificamos se o device possui câmera. Se sim, adicionamos a devida UIAlertAction
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default, handler: { (action: UIAlertAction) in
+                self.selectPicture(sourceType: .camera)
+            })
+            alert.addAction(cameraAction)
+        }
+        
+        //As UIAlertActions de Biblioteca de fotos e Álbum de fotos também são criadas e adicionadas
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action: UIAlertAction) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let photosAction = UIAlertAction(title: "Álbum de fotos", style: .default) { (action: UIAlertAction) in
+            self.selectPicture(sourceType: .savedPhotosAlbum)
+        }
+        alert.addAction(photosAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func selectPicture(sourceType: UIImagePickerControllerSourceType) {
+        //Criando o objeto UIImagePickerController
+        let imagePicker = UIImagePickerController()
+        
+        //Definimos seu sourceType através do parâmetro passado
+        imagePicker.sourceType = sourceType
+        
+        //Definimos a MovieRegisterViewController como sendo a delegate do imagePicker
+        imagePicker.delegate = self
+        
+        //Apresentamos a imagePicker ao usuário
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+
+    @IBAction func addState(_ sender: UIButton) {
+    }
+}
+
+extension ProductRegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //O método abaixo nos trará a imagem selecionada pelo usuário em seu tamanho original
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String: AnyObject]?) {
+        
+        //Iremos usar o código abaixo para criar uma versão reduzida da imagem escolhida pelo usuário
+        let smallSize = CGSize(width: 300, height: 280)
+        UIGraphicsBeginImageContext(smallSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+        
+        //Atribuímos a versão reduzida da imagem à variável smallImage
+        smallImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        ivPicture.image = smallImage //Atribuindo a imagem à ivPoster
+        
+        //Aqui efetuamos o dismiss na UIImagePickerController, para retornar à tela anterior
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ProductRegisterViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return dataSource[row].name
+    }
+}
+
+extension ProductRegisterViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1    //Usaremos apenas 1 coluna (component) em nosso pickerView
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count //O total de linhas será o total de itens em nosso dataSource
+    }
 }
